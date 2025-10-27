513c41475bffe41db3e2cff1d0f7159426b94929
mazyaryousefinia <mazyaryousefinia@gmail.com>
2025-10-27 11:43:11 +0330

    feat(migrations): add cleanup migration for duplicate board versioning

---
+++ server/db/migrations/20250121000000_add_board_versioning.js
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (c) 2024 PLANKA Software GmbH
+ * Licensed under the Fair Use License: https://github.com/plankanban/planka/blob/master/LICENSE.md
+ */
+
+// Temporary dummy file to satisfy Knex validation
+// This file will be deleted after cleanup migration runs
+
+exports.up = async () => {
+  // This migration already ran - do nothing
+};
+
+exports.down = async () => {
+  // Cannot be reversed
+};
