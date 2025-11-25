7838ce1793a714ac6ed553ba2b2d095174cb5f2c
Mehdi Keramati <0mehdikeramati1@gmail.com>
2025-11-25 14:09:21 +0330

    feat: Add admin role check to Document Management button

--- client/src/components/common/HomeActions/DocumentManagementButton.jsx
+++ client/src/components/common/HomeActions/DocumentManagementButton.jsx
@@ -7,9 +7,12 @@ import classNames from 'classnames';
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { Icon } from 'semantic-ui-react';
 
+import { UserRoles } from '../../../constants/Enums';
 import Paths from '../../../constants/Paths';
+import selectors from '../../../selectors';
 
 import styles from './DocumentManagementButton.module.scss';
 
@@ -17,9 +20,16 @@ const DocumentManagementButton = React.memo(() => {
   const [t] = useTranslation();
   const navigate = useNavigate();
 
+  const currentUser = useSelector(selectors.selectCurrentUser);
+  const isAdmin = currentUser && currentUser.role === UserRoles.ADMIN;
+
   const handleClick = useCallback(() => {
     navigate(Paths.DOCUMENT_MANAGEMENT);
   }, [navigate]);
+
+  if (!isAdmin) {
+    return null;
+  }
 
   return (
     <button type="button" className={classNames(styles.button)} onClick={handleClick}>
