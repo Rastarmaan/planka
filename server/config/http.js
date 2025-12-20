52647de0e0550a65402e625f73128a982b451a9d
mazyaryousefiniyaeshad <mazyaryousefiniyaeshad@gmail.com>
2025-12-20 16:21:58 +0330

    add swagger

--- server/config/http.js
+++ server/config/http.js
@@ -9,6 +9,31 @@
  * https://sailsjs.com/config/http
  */
 
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerConfig = require('./swagger');
+
+const swaggerSpec = swaggerJsdoc(swaggerConfig);
+
+module.exports.swaggerSpec = swaggerSpec;
+
+const swaggerAssetPatterns = [
+  '/swagger-ui.css',
+  '/swagger-ui-bundle.js',
+  '/swagger-ui-standalone-preset.js',
+  '/swagger-ui-init.js',
+  '/favicon-32x32.png',
+  '/favicon-16x16.png',
+];
+
+const isSwaggerRequest = (url) => {
+  return (
+    url === '/api-docs' ||
+    url.startsWith('/api-docs/') ||
+    swaggerAssetPatterns.some((pattern) => url === pattern || url.startsWith(pattern))
+  );
+};
+
 module.exports.http = {
   /**
    *
@@ -26,16 +51,49 @@ module.exports.http = {
      * (This Sails app's routes are handled by the "router" middleware below.)
      *
      */
-    // order: [
-    //   'cookieParser',
-    //   'session',
-    //   'bodyParser',
-    //   'compress',
-    //   'poweredBy',
-    //   'router',
-    //   'www',
-    //   'favicon',
-    // ],
+    order: [
+      'cookieParser',
+      'session',
+      'bodyParser',
+      'compress',
+      'poweredBy',
+      'swaggerJson',
+      'swaggerUi',
+      'router',
+      'www',
+      'favicon',
+    ],
+
+    swaggerJson: (req, res, next) => {
+      if (process.env.NODE_ENV !== 'production' && req.url === '/api-docs.json') {
+        res.setHeader('Content-Type', 'application/json');
+        return res.send(swaggerSpec);
+      }
+      return next();
+    },
+
+    swaggerUi: (req, res, next) => {
+      if (process.env.NODE_ENV !== 'production' && isSwaggerRequest(req.url)) {
+        const handlers = swaggerUi.serve.concat(
+          swaggerUi.setup(swaggerSpec, {
+            explorer: true,
+            customSiteTitle: 'PLANKA API Documentation',
+          }),
+        );
+
+        let index = 0;
+        const runNext = (err) => {
+          if (err) return next(err);
+          if (index >= handlers.length) return next();
+          const handler = handlers[index];
+          index += 1;
+          return handler(req, res, runNext);
+        };
+        return runNext();
+      }
+      return next();
+    },
+
     /**
      *
      * The body parser that will handle incoming multipart HTTP requests.
